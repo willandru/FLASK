380cb9078cfa39f5f524383ce252c847335dd986
willandru <willandru@hotmail.com>
2022-09-12 13:32:19 -0500

    Adding index.html

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def index():
 
 	data={
 		'titulo': 'Index',
-		'bienvenido' : '' 
+		'bienvenido' : 'Saludos!' 
 
 	}
 
-	return '<h1>@willandru | Holamundo en Flask</h1>'
+	return render_template('index.html', data=data)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
